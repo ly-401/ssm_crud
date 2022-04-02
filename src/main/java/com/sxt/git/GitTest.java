467896f533cc90d430f30e134e211f22387469e6
future <future@shangui.com>
2022-04-02 18:20:32 +0800

    push commit

--- src/main/java/com/sxt/git/GitTest.java
+++ src/main/java/com/sxt/git/GitTest.java
@@ -16,7 +16,7 @@ public class GitTest {
         System.out.println("master test！");
         System.out.println("hello git4 hot-fix");
         System.out.println("hot-fix test");
-        System.out.println("push test！");
+        System.out.println("push test！ ");
 
     }
 
