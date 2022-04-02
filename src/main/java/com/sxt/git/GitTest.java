dff5d67b80231a0edacaaaad1f28cedd8da043e0
ly-401 <64905050+ly-401@users.noreply.github.com>
2022-04-02 20:18:04 +0800

    pull commit

--- src/main/java/com/sxt/git/GitTest.java
+++ src/main/java/com/sxt/git/GitTest.java
@@ -17,7 +17,7 @@ public class GitTest {
         System.out.println("hello git4 hot-fix");
         System.out.println("hot-fix test");
         System.out.println("push test！ ");
-
+        System.out.println("pull test！ ");
     }
 
 }
